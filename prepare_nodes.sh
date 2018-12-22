cb7d5c35fd164830de2bf5efa0c9de1ea6818bfd
hadoop <hadoop@vm0.ansible.lab>
2018-12-21 20:33:37 -0500

    h

--- prepare_nodes.sh
+++ prepare_nodes.sh
@@ -3,6 +3,6 @@
 source $(dirname "${BASH_SOURCE[0]}")/set_cloud.sh
 
 ansible-playbook "playbooks/prepare_nodes.yml" \
-                 --inventory="inventory/${cloud_to_use}" \
+#                 --inventory="inventory/${cloud_to_use}" \
                  --extra-vars="cloud_name=${cloud_to_use}" \
                  "$@"
